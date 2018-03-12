f50967f1a5b9571d375ca6b525ebda5195fe4137
dcamriff <driffelatl@gmail.com>
2018-03-12 15:37:47 -0400

    Refactoring to add ProjectData as separate component

---
+++ src/components/ProjectData.js
@@ -0,0 +1,56 @@
+export default [
+{
+    name: "Capstone: Barista Goals",
+    description: "Project 4: A training and social app for baristas, that captures field notes pertaining to various specialty coffee subjects.",
+    technology: "React / Ruby on Rails / PostgreSQL",
+    github: "https://github.com/dcamriff/barista-goals",
+    weblink: "https://barista-goals.herokuapp.com/",
+    imageUrl: "https://i.imgur.com/mlTyXWm.png",
+},
+
+{
+    name: "1 | Jeopardy Game",
+    description: "Project 1: An app in the classic style of Jeopardy centered around trivia tailored towards kids.",
+    technology: "HTML / CSS / Javascript",
+    github: "https://github.com/dcamriff/jeopardy-game-wdi",
+    weblink: "http://jeopardy-for-kids-game-5e11a7.bitballoon.com/",
+    imageUrl: "https://i.imgur.com/Er44s8D.png",
+},
+
+{
+    name: "2 | Chore Rewarder",
+    description: "Project 2: An app that allows family members to keep track in the sharing of family chores.",
+    technology: "MongoDB / Express / React / Node.js",
+    github: "https://github.com/dcamriff/wdi-project-3-dcr",
+    weblink: "https://project-3-dcr.herokuapp.com/users",
+    imageUrl: "https://i.imgur.com/Ue0nO1S.png",
+},
+
+{
+    name: "3 | Gig Posters",
+    description: "Project 3: A site that allows users to upload and track details of their band poster collections.",
+    technology: "MongoDB / Express / Node.js",
+    github: "https://github.com/dcamriff/gig-posters-wdi",
+    weblink: "https://gig-posters-wdi.herokuapp.com/",
+    imageUrl: "https://i.imgur.com/RWjdxA0.png",
+},
+
+{
+    name: "4/Group | Vojâgo Travel",
+    description: "Group Project: Worked in a team environment to co-develop a social app centered around travel experiences in major cities, where users can post images and comments. Pair-programmed, primarily on front-end development in React.",
+    technology: "Group Project: React / Ruby on Rails / PostgreSQL",
+    github: "",
+    weblink: "http://voyago.herokuapp.com/",
+    imageUrl: "https://i.imgur.com/qefJNAY.png",
+},
+
+{
+    name: "Portfolio Site",
+    description: "Link to my github repo for my portfolio site.",
+    technology: "React / Styled-Components",
+    github: "https://github.com/dcamriff/portfolio",
+    weblink: "https://github.com/dcamriff/dcamriff_portfolio/",
+    imageUrl: "https://i.imgur.com/KqX07mk.png",
+}
+
+]
